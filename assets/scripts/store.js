ed6b2e7ef3d5ace149904d424b8ba6c7587ed1dc
elainechan55 <emchan55@gmail.com>
2021-04-26 22:41:51 -0500

    Add 'tasks' Array to be Stored
    Add comments in store.js for notes

--- assets/scripts/store.js
+++ assets/scripts/store.js
@@ -1,6 +1,33 @@
 'use strict'
 
+// in store: user, tasks
+/*
+{
+"user": {
+    "_id": "<id>",
+    "email": "<email>",
+    "createdAt": "<date>",
+    "updatedAt": "<date>",
+    "__v":0,
+    "token": "<token>"
+  }
+}
+[{
+  task = {
+    "_id":
+    "title":
+    "description":
+    "isComplete":
+    "owner":
+    "createdAt":
+    "updatedAt":
+    "_v:0"
+  }
+}]
+*/
+
 const store = {
+  tasks: []
 }
 
 module.exports = store
